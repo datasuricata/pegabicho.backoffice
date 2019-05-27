1eeaeae1cd3fc0b229d881d8874eb522bc172266
Álvaro Cintra <alvarocintra@hotmail.com>
2019-05-27 17:02:37 -0300

    Commit inicial para testes adicionado LoginController

---
+++ pegabicho.backoffice/pegabicho.backoffice/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pegabicho.backoffice.Controllers
+{
+    public class LoginController : CoreController
+    {
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string login, string senha)
+        {
+            var userLoginCommand = new LoginDTO();
+            userLoginCommand.Login = login;
+            userLoginCommand.Senha = senha;
+            var request = await Post<T>("/api/Login/backoffice", LoginDTO);
+            return View();
+        }
+    }
+}
